3a35eab410338f17aab4f4dae23412e7ebf335d1
Nurmuhammad-TUIT <nurmuhammad09062020@gmail.com>
2022-03-06 19:45:09 +0300

    Initial commit

---
+++ Desktop/Najot ta'lim/3-oy/3-oy-exam/js/js-2/tasktwo.js
@@ -0,0 +1,15 @@
+var arr = [true, true, false, false, true];
+
+var arr = [];
+
+function countTrue (arr) {
+   for (var i of arr ) {
+      if (i == true ) {
+         arr.push
+      }
+   }
+
+   return arr.length;
+}
+
+console.log(countTrue(arr));
